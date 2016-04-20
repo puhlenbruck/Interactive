d7f05d9a8d897f42a8f494238871262fa8a92e2c
Peter Uhlenbruck <peuhlenb@gmail.com>
2016-04-19 22:04:44 -0400

    formatting changes

--- src/main/kotlin/com/puhlen/interactive/Menu.kt
+++ src/main/kotlin/com/puhlen/interactive/Menu.kt
@@ -4,36 +4,38 @@
  */
 package com.puhlen.interactive
 
-class Menu(){
+class Menu() {
     var title = ""
     private var items: MutableList<MenuItem> = mutableListOf()
-    fun addItem(item: MenuItem):Boolean{
+    fun addItem(item: MenuItem): Boolean {
         return items.add(item)
     }
 
-    fun render(){
+    fun render() {
         Output.printStream.println(title)
-        for(i in items.indices){
-            Output.printStream.println("${i+1}) ${items[i]}")
+        for (i in items.indices) {
+            Output.printStream.println("${i + 1}) ${items[i]}")
         }
     }
 }
 
-class MenuItem(val message: String){
-    var selectionHandler:SelectionHandler = DefaultSelectionHandler;
-    internal fun select(){
+class MenuItem(val message: String) {
+    var selectionHandler: SelectionHandler = DefaultSelectionHandler;
+    internal fun select() {
         selectionHandler.select()
     }
-    override fun toString(): String{
+
+    override fun toString(): String {
         return message
     }
 
 }
 
-interface SelectionHandler{
+interface SelectionHandler {
     fun select();
 }
 
-object DefaultSelectionHandler:SelectionHandler{
-    override fun select() {}
+object DefaultSelectionHandler : SelectionHandler {
+    override fun select() {
+    }
 }
