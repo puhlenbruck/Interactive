ca9914c8234dbe01e55765769d99ad103ef91de0
Peter Uhlenbruck <peuhlenb@gmail.com>
2016-04-21 22:07:47 -0400

    Render now accepts input and launches the selection

---
+++ src/main/kotlin/com/puhlen/interactive/Input.kt
@@ -0,0 +1,13 @@
+/*
+ * MIT License
+ * Copyright (c) 2016 Peter Uhlenbruck
+ */
+
+package com.puhlen.interactive
+
+/**
+ * Created by peter on 21/04/16.
+ */
+object Input {
+    var inputSream = System.`in`;
+}
--- src/main/kotlin/com/puhlen/interactive/Menu.kt
+++ src/main/kotlin/com/puhlen/interactive/Menu.kt
@@ -4,17 +4,33 @@
  */
 package com.puhlen.interactive
 
+import java.util.*
+
 class Menu() {
     var title = ""
-    private var items: MutableList<MenuItem> = mutableListOf()
+    private val items: MutableList<MenuItem> = mutableListOf()
     fun addItem(item: MenuItem): Boolean {
         return items.add(item)
     }
 
     fun render() {
-        Output.printStream.println(title)
-        for (i in items.indices) {
-            Output.printStream.println("${i + 1}) ${items[i]}")
+        var gettingInput = true
+        while(gettingInput) {
+            Output.printStream.println(title)
+            for (i in items.indices) {
+                Output.printStream.println("${i + 1}) ${items[i]}")
+            }
+            val read = Scanner(Input.inputSream)
+            val line = read.nextLine().trim();
+            if (line.matches(Regex("[0-9]+"))) {
+                val selection = line.toInt() - 1
+                if(selection in items.indices){
+                    items[selection].select()
+                    gettingInput = false
+                    return
+                }
+            }
+            Output.printStream.println("please enter a number between 1 and ${items.size}")
         }
     }
 }
